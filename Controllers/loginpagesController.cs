f0af0e52d7dbd8563a8202dfc65e0c6e8bddf820
ThrinadhPalvai <thrinadhpalvai1106@gmail.com>
2025-02-27 16:55:46 +0530

    Add project files.

---
+++ Controllers/loginpagesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using LoginPagePeoject;
+
+namespace LoginPagePeoject.Controllers
+{
+    public class loginpagesController : Controller
+    {
+        private proteckhydbEntities db = new proteckhydbEntities();
+
+        // GET: loginpages
+        public ActionResult Index()
+        {
+            return View(db.loginpages.ToList());
+        }
+
+        // GET: loginpages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            loginpage loginpage = db.loginpages.Find(id);
+            if (loginpage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loginpage);
+        }
+
+        // GET: loginpages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: loginpages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Username,Password")] loginpage loginpage)
+        {
+            //if (ModelState.IsValid)
+            //{
+            //    db.loginpages.Add(loginpage);
+            //    db.SaveChanges();
+            //    return RedirectToAction("Index");
+            //}
+
+            var user=db.loginpages.FirstOrDefault(x=>x.Username==loginpage.Username&& x.Password==loginpage.Password);
+            if (user != null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //ModelState.AddModelError("", "Invalid Credentials");
+                ViewBag.validation = "Invalid Credentials";
+            }
+            return View(loginpage);
+        }
+
+        // GET: loginpages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            loginpage loginpage = db.loginpages.Find(id);
+            if (loginpage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loginpage);
+        }
+
+        // POST: loginpages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Username,Password")] loginpage loginpage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(loginpage).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(loginpage);
+        }
+
+        // GET: loginpages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            loginpage loginpage = db.loginpages.Find(id);
+            if (loginpage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loginpage);
+        }
+
+        // POST: loginpages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            loginpage loginpage = db.loginpages.Find(id);
+            db.loginpages.Remove(loginpage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return View();
+        }
+    }
+}
---
+++ Controllers/registrationsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using LoginPagePeoject;
+
+namespace LoginPagePeoject.Controllers
+{
+    public class registrationsController : Controller
+    {
+        private proteckhydbEntities db = new proteckhydbEntities();
+
+        // GET: registrations
+        public ActionResult Index()
+        {
+            var registrations = db.registrations.Include(r => r.DesignTb);
+            return View(registrations.ToList());
+        }
+
+        // GET: registrations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registration registration = db.registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // GET: registrations/Create
+        public ActionResult RegCreate()
+        {
+            ViewBag.Designation = new SelectList(db.DesignTbs, "Id", "Designation");
+            return View();
+        }
+
+        // POST: registrations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegCreate(registration reg, loginpage loginPage)
+
+        {
+            db.registrations.Add(reg);
+            db.loginpages.Add(loginPage);
+            loginPage.Id = reg.Id;
+            loginPage.Username = reg.Username;
+            loginPage.Password = reg.Password;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: registrations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registration registration = db.registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Designation = new SelectList(db.DesignTbs, "Id", "Designation", registration.Designation);
+            return View(registration);
+        }
+
+        // POST: registrations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Username,Password,DateofBirth,Email,PhoneNumber,Address,Designation")] registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Designation = new SelectList(db.DesignTbs, "Id", "Designation", registration.Designation);
+            return View(registration);
+        }
+
+        // GET: registrations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registration registration = db.registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: registrations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            registration registration = db.registrations.Find(id);
+            db.registrations.Remove(registration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
